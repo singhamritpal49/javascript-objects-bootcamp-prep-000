806acd71c0d3efcfb8ea0f4d73d383c7d98f3eca
Amritpal Singh <singhamritpal49@gmail.com>
2019-04-12 05:08:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {
 raikoti: "Munda Ambarsariya"
 };
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.Slowdive = 'Singh',
-  playlist.Amrit= 'Sometimes'
+  playlist.Slowdive: 'Singh',
+  playlist.Amrit: 'Sometimes'
   return updatePlaylist
 }
