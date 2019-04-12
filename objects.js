9d3087a7dff80effed5e90c1d462dedf71695b84
Amritpal Singh <singhamritpal49@gmail.com>
2019-04-12 05:08:00 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,10 +4,6 @@ raikoti: "Munda Ambarsariya"
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist["Slowdive"] = 'Singh',
   playlist['My Bloody Valentine'] = 'Sometimes'
+  return updatePlaylist
 }
 
-describe('updatePlaylist(playlist, artistName, songTitle)', () => {
-  before(() => {
-    playlist['Slowdive'] = 'Alison'
-    playlist['My Bloody Valentine'] = 'Sometimes'
-  })
