172112461de7a40f31c186d42eb78f6352035ad6
Amritpal Singh <singhamritpal49@gmail.com>
2019-04-12 05:07:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,5 +2,12 @@ var playlist = {
 raikoti: "Munda Ambarsariya"
 };
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist["Phil Ochs"]
+  playlist["Slowdive"] = 'Singh',
+  playlist['My Bloody Valentine'] = 'Sometimes'
 }
+
+describe('updatePlaylist(playlist, artistName, songTitle)', () => {
+  before(() => {
+    playlist['Slowdive'] = 'Alison'
+    playlist['My Bloody Valentine'] = 'Sometimes'
+  })
