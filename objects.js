9387c12ef802d80415c5c609901620d51968267f
Amritpal Singh <singhamritpal49@gmail.com>
2019-04-12 05:13:03 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = {
 raikoti: "Munda Ambarsariya"
 };
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = "songTitle"
+  playlist[artistName] = "songTitle"
   return playlist
 }
