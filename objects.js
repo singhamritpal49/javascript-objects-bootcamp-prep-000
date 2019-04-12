e576d35956548673f4dd46a101f3e40503d41b2f
Amritpal Singh <singhamritpal49@gmail.com>
2019-04-12 05:25:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,3 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   //playlist.artistName = songTitle
   return playlist
 }
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName] = songTitle
+}
